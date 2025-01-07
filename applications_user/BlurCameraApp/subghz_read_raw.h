cfa12083134f183f61d4f5eaa198d3dfc1ae9a8a
theoneforever32 <theoneforever32@gmail.com>
2025-01-07 09:44:47 -0600

    committed

--- applications_user/BlurCameraApp/subghz_read_raw.h
+++ applications_user/BlurCameraApp/subghz_read_raw.h
@@ -2,7 +2,7 @@
 
 #include <gui/view.h>
 #include "../lib/helpers/subghz/subghz_types.h"  // Update the include path
-#include "../helpers/subghz_custom_event.h"
+#include "../lib/helpers/subghz/subghz_custom_event.h"  // Update the include path
 
 #define SUBGHZ_RAW_THRESHOLD_MIN -90.0f
 
--- applications_user/BlurCameraApp/transmitter.h
+++ applications_user/BlurCameraApp/transmitter.h
@@ -2,7 +2,7 @@
 
 #include <gui/view.h>
 #include "../lib/helpers/subghz/subghz_types.h"  // Update the include path
-#include "../helpers/subghz_custom_event.h"
+#include "../lib/helpers/subghz/subghz_custom_event.h"  // Update the include path
 
 typedef struct SubGhzViewTransmitter SubGhzViewTransmitter;
 
